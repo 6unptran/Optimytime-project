5ad39fe5f1f779538a7f242059434056dd84fac2
6unptran <155554002+6unptran@users.noreply.github.com>
2024-05-12 17:53:47 +0700

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login',views.login,name='login'),
+    path('note',views.note,name='note'),
     path('requests_msg/', views.requests_msg, name='requests_msg')
 ]
